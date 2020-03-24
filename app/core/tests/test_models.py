b4abce00ad341ed8097dd652415d6e0f3a61bc4a
aldhi <aldhi@sekolahmu.co.id>
2020-03-24 22:03:28 +0700

    setup postgresql

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successfull(self):
@@ -8,10 +9,9 @@ class ModelTests(TestCase):
         email = 'test@londonappdev.com'
         password = 'Testpass123'
         user = get_user_model().objects.create_user(
-            email = email,
-            password = password
+            email=email,
+            password=password
         )
-
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
@@ -19,7 +19,6 @@ class ModelTests(TestCase):
         """Test the email for new user is normalized"""
         email = 'test@GMAIL.COM'
         user = get_user_model().objects.create_user(email, 'test123')
-
         self.assertEqual(user.email, email.lower())
 
     def test_new_user_invalid_email(self):
