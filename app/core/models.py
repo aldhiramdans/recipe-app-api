e6f4b7411ca6be55577aeecc8807d7df1dd2ddd2
aldhi <aldhi@sekolahmu.co.id>
2020-04-12 23:33:12 +0700

    refactor test and ingridients

--- app/core/models.py
+++ app/core/models.py
@@ -42,9 +42,9 @@ class Tag(models.Model):
     """Tag to be user for a recipe"""
     name = models.CharField(max_length=255)
     user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
-    )
+            settings.AUTH_USER_MODEL,
+            on_delete=models.CASCADE
+            )
 
     def __str__(self):
         return self.name
@@ -54,9 +54,9 @@ class Ingridient(models.Model):
     """Ingridient to be use in recipe"""
     name = models.CharField(max_length=255)
     user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
-    )
+            settings.AUTH_USER_MODEL,
+            on_delete=models.CASCADE
+            )
 
     def __str__(self):
         return self.name
